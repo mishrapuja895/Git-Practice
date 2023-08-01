1e7dc9f9f2c920e74040cea93cf4502425e06b8b
mishrapuja <mishrapuja895>
2023-08-01 11:46:17 +0530

    commited

--- Bandar.java
+++ Bandar.java
@@ -9,4 +9,17 @@ public int getsum(int n1,int n2)
     return n1+n2;
 }
 }
+public Class BandarDriver
+{
+    public static void main(string[] args)
+    {
+        Scanner s= new scanner(System.in);
+        system.out.println("Enter 2 numbers to find sum");
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        BandarImp b= new BandarImp();
+        int sum= c.getSum(n1,n2);
+        System.out.println("sum");
+    }
+}
 
